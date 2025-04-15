22ce5e03b9c1c8bad5597fc6870ac0336ea4cc72
tuantq <truongquoctuan1409@gmail.com>
2025-04-15 22:03:49 +0700

    [update]: add security with keycloak

---
+++ tms-be/tms-statistic/src/main/java/com/bigbird/tmsstatistic/security/CustomJwtAuthenticationConverter.java
@@ -0,0 +1,33 @@
+package com.tms_statistic.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CustomJwtAuthenticationConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        return extractAuthorities(jwt);
+    }
+
+    private Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaimAsMap("realm_access");
+
+        List<String> roles = List.of();
+        if (realmAccess != null && realmAccess.containsKey("roles")) {
+            roles = (List<String>) realmAccess.get("roles");
+        }
+
+        Collection<GrantedAuthority> result = roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+        return result;
+    }
+}
