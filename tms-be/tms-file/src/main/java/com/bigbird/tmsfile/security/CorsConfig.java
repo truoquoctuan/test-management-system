eaa1ca8ab3e45434201b0863eb1fab9228075596
tuantq <truongquoctuan1409@gmail.com>
2025-05-08 16:40:25 +0700

    [update]: add security in tms-file

---
+++ tms-be/tms-file/src/main/java/com/bigbird/tmsfile/security/CorsConfig.java
@@ -0,0 +1,25 @@
+//package com.tms_file.security;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Value("${domain.fe}")
+//    private String domainFE;
+//
+//    @Value("${ip.fe}")
+//    private String ipFE;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/graphql/**")
+//                .allowedOrigins(domainFE, ipFE)
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
