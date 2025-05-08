ea14950bbe686eab80c246d7c4994080bf785a57
tuantq <truongquoctuan1409@gmail.com>
2025-05-08 16:41:08 +0700

    [update]: add controller in tms-file

---
+++ tms-be/tms-file/src/main/java/com/bigbird/tmsfile/controller/RabbitController.java
@@ -0,0 +1,24 @@
+//package com.tms_file.controller;
+//
+//import com.tms_file.service.RabbitProducer;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class RabbitController {
+//
+//    @Autowired
+//    private RabbitProducer rabbitProducer;
+//
+//    public RabbitController(RabbitProducer rabbitProducer) {
+//        this.rabbitProducer = rabbitProducer;
+//    }
+//
+//    @GetMapping("/send")
+//    public String getMessage(@RequestParam String message) {
+//        rabbitProducer.sendMessage(message);
+//        return "Message sent from tms-file: " + message;
+//    }
+//}
